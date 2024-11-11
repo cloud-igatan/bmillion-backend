43a3acc40e24be0f7a53da0b5e567095d2b9faa5
Yoo Jeong <04dbwjd@gmail.com>
2024-11-11 15:12:54 +0900

    first commit

---
+++ src/main/java/com/example/bmillion_backend/BmillionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmillion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmillionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmillionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bmillion_backend/BmillionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmillion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmillionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
