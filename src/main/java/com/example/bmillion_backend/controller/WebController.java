8229631ab8ecdb4bae3073867db35b33bba332f3
Yoo Jeong <04dbwjd@gmail.com>
2024-12-10 23:22:34 +0900

    feat: react controller 추가

---
+++ src/main/java/com/example/bmillion_backend/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.example.bmillion_backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping(value =  {"", "/login", "/ThirdPage", "/Calender", "/ShowDiary"})
+    public String forward() {
+        return "forward:/index.html";
+    }
+
+}
