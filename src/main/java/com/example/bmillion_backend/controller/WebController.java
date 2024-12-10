a0bb11b90084b7783846d6911bf5f880ec4aaf32
Yoo Jeong <04dbwjd@gmail.com>
2024-12-11 01:38:54 +0900

    fix: react controller 재수정

--- src/main/java/com/example/bmillion_backend/controller/WebController.java
+++ src/main/java/com/example/bmillion_backend/controller/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping({"", "/", "/login", "/ThirdPage", "/Calender", "/ShowDiary"})
+    @GetMapping({"", "/", "/login", "/ThirdPage", "/Calender", "/ShowDiary", "/error"})
     public String index() {
         return "index.html";
     }
